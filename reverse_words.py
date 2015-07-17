8524f2cdaa48716c74bd03a64bf960c25c8bd9b5
Sam Schmalzried <samschmalz@users.noreply.github.com>
2015-07-17 12:41:53 -0500

    Update reverse_words.py

--- reverse_words.py
+++ reverse_words.py
@@ -3,9 +3,5 @@ test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
     words = test.split(" ")
     words = words[::-1]
-    final = ""
-    for item in words:
-        final += item
-        final += " "
-    final = final[:-1]
+    final = " ".join(words)
     print final
