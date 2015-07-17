818ca30adab9bb3a54c85529432659a22861f37f
Sam Schmalzried <samschmalz@users.noreply.github.com>
2015-07-17 12:49:39 -0500

    Update reverse_words.py
    added code to remove a newline character that was added when the test item was split.

--- reverse_words.py
+++ reverse_words.py
@@ -2,6 +2,9 @@ import sys
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
     words = test.split(" ")
-    words = words[::-1]
+    words.reverse()
+    first = words[0]
+    first = first[:-1]
+    words[0] = first
     final = " ".join(words)
     print final
